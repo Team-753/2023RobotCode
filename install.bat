b908bc01de7732f2625b215431c2e82285ad9e77
jserra99 <progamer5051@gmail.com>
2023-01-22 22:07:35 -0800

    added pathplannerlib

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-py -3 -m pip install --upgrade robotpy[all] robotpy-ctre robotpy-navx robotpy-rev robotpy-apriltag robotpy-photonvision
+py -3 -m pip install --upgrade robotpy[all] robotpy-ctre robotpy-navx robotpy-rev robotpy-apriltag robotpy-photonvision robotpy-pathplannerlib
 PAUSE
-echo to update the robot do the following: "py -3 -m robotpy_installer download robotpy[all] robotpy-ctre robotpy-navx robotpy-rev robotpy-apriltag robotpy-photonvision" then replace download with install
+echo to update the robot do the following: "py -3 -m robotpy_installer download robotpy[all] robotpy-ctre robotpy-navx robotpy-rev robotpy-apriltag robotpy-photonvision robotpy-pathplannerlib" then replace download with install
