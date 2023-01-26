6d929084f3613acf8ec8ae01c7347ffa547680ac
Joe <73498493+jserra99@users.noreply.github.com>
2023-01-25 22:35:44 -0800

    i am going insane

--- controlsystems/commandController.py
+++ controlsystems/commandController.py
@@ -1,6 +1,27 @@
+from wpimath import geometry
+from subsystems.arm import Arm
+from subsystems.mandible import Mandible
+from math import pi
+
 class CommandController:
     ''' This exists because I didn't want to take the time to learn wpilib commands, also I hate myself so yeah...
     Anyway, this is basically what acts on those fancy macro buttons we have this year to automate large parts of the game.'''
+    placementConstants = { # the positioning constants associated with each and every command 
+        "leftGrid": {
+            "topLeftCone": ["highCone", "cubeMode", geometry.Pose2d(geometry.Translation2d(), geometry.Rotation2d(pi))] # convention is as follows: arm key, mandible key, chassis location
+        },
+        "midGrid": {
+            
+        },
+        "rightGrid": {
+            
+        }
+    }
     
+    macros = { # for things either the driver or auto may want to do at some point, not a full automation, just doing something...
+        
+    }
     def __init__(self) -> None:
         pass
+    
+    
---
+++ subsystems/mandible.py
@@ -0,0 +1,28 @@
+import wpilib
+
+class Mandible:
+    ''''''
+    
+    def __init__(self) -> None:
+        pass
+    
+    def doSomething(self, todo: str):
+        pass
+    
+    def coneMode(self):
+        ''' Pretty self-explanatory '''
+        
+    def cubeMode(self):
+        ''' Pretty self-explanatory '''
+        
+    def intake(self):
+        ''' NOTE: Needs to check whether or not game piece is fully in the mandible, distance sensor??? will return false until true. '''
+        
+    def outtake(self):
+        ''' Pretty self-explantory '''
+        
+    def coast(self):
+        ''''''
+    
+    def brake(self):
+        ''''''
